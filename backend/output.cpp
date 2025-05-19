0ab9e183fac5c37c58b62384d8f54eb8c5d4425e
Harshit Gupta <iamharshit999@gmail.com>
2025-05-19 22:16:04 +0530

    update

---
+++ backend/output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void is_odd_or_even(int number) {
+    if (((number % 2) == 0)) {
+        return Even;
+    }
+    else {
+        return Odd;
+    }
+}
